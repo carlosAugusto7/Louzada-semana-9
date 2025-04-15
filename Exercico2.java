24fabf7d95b7a865ad163bda05f05c28b11465aa
Carlos Augusto <carlosaugusto20003@gmail.com>
2025-04-15 14:37:05 -0300

    Create Exercico2.java

---
+++ Exercico2.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class aula {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> nomes = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Digite aqui o nome: ");
+            String nome = scanner.nextLine();
+            nomes.add(nome);
+        }
+
+        System.out.print("Digite aqui o nome que deseja remover: ");
+        String remover = scanner.nextLine();
+
+        if (nomes.contains(remover)) {
+            nomes.remove(remover);
+            System.out.println("Nome removido!");
+        } else {
+            System.out.println("Nome não encontrado na lista.");
+        }
+
+        System.out.println("Nomes restantes:");
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+        scanner.close();
+    }
+}
+
+///Exercício feito às 14:36 no dia 15/04/2025
