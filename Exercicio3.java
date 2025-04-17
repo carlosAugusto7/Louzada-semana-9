f8f17b427a3c7bc7614f8668963c41780a1a7752
Carlos Augusto <carlosaugusto20003@gmail.com>
2025-04-17 14:28:46 -0300

    Create Exercicio3.java

---
+++ Exercicio3.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class aula {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Double> numeros = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Digite o número: ");
+            Double numero = scanner.nextDouble();
+
+                if (numero == -1){
+                    break;  
+                 }
+            numeros.add(numero);
+        
+        }
+
+        Double soma = 0.0;
+        for (Double numero : numeros){  
+           soma += numero;
+          }
+
+          Double media = soma / numeros.size();
+
+          System.out.println("Soma: " + soma);
+          System.out.println("Média: " + media);
+
+         scanner.close();
+
+    }
+
+}
+
+/// Feito no dia 17/04/2025 as 14:28
